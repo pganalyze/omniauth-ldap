2d37e518d0613688b8e0d6ad66653ecc37c0026c
Lukas Fittl <lukas@fittl.com>
2025-03-06 17:32:34 -0800

    Add support for Rack 3.X
    This requires using request.params instead of accessing the parameters
    directly via []. See https://github.com/omniauth/omniauth/issues/1135
    
    Fixes "undefined method `[]' for an instance of Rack::Request: NoMethodError"

--- lib/omniauth/strategies/ldap.rb
+++ lib/omniauth/strategies/ldap.rb
@@ -46,10 +46,10 @@ module OmniAuth
         return fail!(:invalid_request_method) unless valid_request_method?
         return fail!(:missing_credentials) if missing_credentials?
         begin
-          @ldap_user_info = @adaptor.bind_as(:filter => filter(@adaptor), :size => 1, :password => request['password'])
+          @ldap_user_info = @adaptor.bind_as(:filter => filter(@adaptor), :size => 1, :password => request_params['password'])
 
           unless @ldap_user_info
-            return fail!(:invalid_credentials, InvalidCredentialsError.new("Invalid credentials for #{request['username']}"))
+            return fail!(:invalid_credentials, InvalidCredentialsError.new("Invalid credentials for #{request_params['username']}"))
           end
 
           @user_info = self.class.map_user(@@config, @ldap_user_info)
@@ -61,10 +61,10 @@ module OmniAuth
 
       def filter(adaptor)
         if adaptor.filter and !adaptor.filter.empty?
-          username = Net::LDAP::Filter.escape(@options[:name_proc].call(request['username']))
+          username = Net::LDAP::Filter.escape(@options[:name_proc].call(request_params['username']))
           Net::LDAP::Filter.construct(adaptor.filter % { username: username })
         else
-          Net::LDAP::Filter.equals(adaptor.uid, @options[:name_proc].call(request['username']))
+          Net::LDAP::Filter.equals(adaptor.uid, @options[:name_proc].call(request_params['username']))
         end
       end
 
@@ -107,8 +107,12 @@ module OmniAuth
       end
 
       def missing_credentials?
-        request['username'].nil? or request['username'].empty? or request['password'].nil? or request['password'].empty?
+        request_params['username'].nil? or request_params['username'].empty? or request_params['password'].nil? or request_params['password'].empty?
       end # missing_credentials?
+
+      def request_params
+        request.respond_to?(:params) ? request.params : request
+      end
     end
   end
 end
